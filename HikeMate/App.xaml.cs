83473595754c3119928dc730ec02d4d19f59eb7f
hirucool <viranja@live.com>
2015-01-10 20:37:05 +0530

    <azure1>

--- HikeMate/App.xaml.cs
+++ HikeMate/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -26,6 +27,9 @@ namespace HikeMate
     public sealed partial class App : Application
     {
         private TransitionCollection transitions;
+
+        public static MobileServiceClient MobileService = new MobileServiceClient(
+         "http://localhost:59477");
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
---
+++ HikeMate/Controller/SyncLocationData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using HikeMate.Models;
+
+namespace HikeMate.Controller
+{
+    class SyncLocationData
+    {
+        private MobileServiceCollection<Location, Location> LocationItems;
+        private IMobileServiceTable<Location> LocationTable = App.MobileService.GetTable<Location>();
+
+        public async void InsertLocationItem(Location locationData)
+        {
+            await LocationTable.InsertAsync(locationData);
+            LocationItems.Add(locationData);
+        }
+
+        public async void checkInserted(System.ComponentModel.AsyncCompletedEventArgs actionComplete)
+        {
+            LocationItems = await LocationTable.ToCollectionAsync();
+            if (LocationItems != null)
+            {
+                
+            }
+        }
+
+    }
+}
